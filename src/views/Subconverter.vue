7f777a4f2c538910a97618653737766cb1df5494
citylonlyTan <115676724+citylonlyTan@users.noreply.github.com>
2026-09-05 23:13:21 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,12 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
+          "我的私有 NAS 后端": "https://subcon.833933.xyz:65525",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
           "肥羊提供-备用后端": "https://api.v1.mk",
         },
         backendOptions: [
+          { value: "https://subcon.833933.xyz:65525" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
