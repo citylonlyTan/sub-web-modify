3c9a80e212e050616c567b84549e9a6f4f449b9e
citylonlyTan <115676724+citylonlyTan@users.noreply.github.com>
2026-09-05 23:29:12 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,7 +348,7 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
-          "我的私有 NAS 后端": "https://subcon.833933.xyz:65525",
+          "Citylonly私有NAS后端": "https://subcon.833933.xyz:65525",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
@@ -792,7 +792,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? defaultBackend : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
         excludeRemarks: "",
